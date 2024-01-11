2ed8658b7f3824589a94b75a3925717b99c96b82
Rogério Soares <rgrsoares@yahoo.com.br>
2024-01-11 10:25:10 -0300

    :star: Finish the project

--- routes/routes.js
+++ routes/routes.js
@@ -10,5 +10,9 @@ routes.get('/', (req, res)=> {
 })
 
 routes.get('/clients', clients.findAll);
+routes.post("/clients", clients.addClient);
+routes.get("/clients/:id", clients.findClient);
+routes.put("/clients/:id", clients.updateClient);
+routes.delete("/clients/:id", clients.deleteClient);
 
 export { routes as default };
--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,8 +1,51 @@
 import ClientRepository from '../models/clientsModel.js';
 
-async function findAll(req, res) {
-    const clients = await ClientRepository.findAll();
-    res.json(clients);
+function findAll(req, res) {
+    ClientRepository.findAll()
+        .then((result) => res.json(result));
 }
 
-export default { findAll };
+function findClient(req, res) {
+    ClientRepository.findByPk(req.params.id)
+        .then((result) => res.json(result))
+}
+
+async function addClient(req, res) {
+    try {
+        const result = await ClientRepository.create({
+            name: req.body.name,
+            email: req.body.email,
+        });
+        res.json(result);
+    } catch (error) {
+        console.error('Error in addClient:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+async function updateClient(req, res) {
+    await ClientRepository.update(
+        {
+            name: req.body.name,
+            email: req.body.email
+        },
+        {
+            where: {
+                id: req.params.id,
+            }
+        }
+    );
+    ClientRepository.findByPk(req.params.id)
+        .then((result) => res.json(result));
+}
+
+async function deleteClient(req, res) {
+    await ClientRepository.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    ClientRepository.findAll().then((result) => res.json(result))
+}
+
+export default { findAll, addClient, findClient, updateClient, deleteClient }
